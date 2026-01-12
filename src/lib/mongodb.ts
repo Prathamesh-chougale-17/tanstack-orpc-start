8565e7924afb0220fa8e1de0a1943696318c969c
Prathamesh Chougale <prathamesh17170@gmail.com>
2026-01-13 00:12:34 +0530

    feat: add @t3-oss/env-core for environment variable management and update MongoDB connection logic

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '@/env'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MONGODB_URI to .env.local')
-}
-
-const uri = process.env.MONGODB_URI
+const uri = env.MONGODB_URI
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,7 +13,7 @@ const options = {
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
-if (process.env.NODE_ENV === 'development') {
+if (env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   const globalWithMongo = global as typeof globalThis & {
